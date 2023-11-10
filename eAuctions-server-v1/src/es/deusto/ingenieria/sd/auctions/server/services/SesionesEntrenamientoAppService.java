f2d2a965adb592eed5d45b6a015b98f3a9446d82
XabiLorenzo <xabier.lorenzo@opendeusto.es>
2023-11-10 11:47:33 +0100

    SEappservice

--- eAuctions-server-v1/src/es/deusto/ingenieria/sd/auctions/server/services/SesionesEntrenamientoAppService.java
+++ eAuctions-server-v1/src/es/deusto/ingenieria/sd/auctions/server/services/SesionesEntrenamientoAppService.java
@@ -3,125 +3,70 @@ package es.deusto.ingenieria.sd.auctions.server.services;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import es.deusto.ingenieria.sd.auctions.server.data.domain.Article;
-import es.deusto.ingenieria.sd.auctions.server.data.domain.Bid;
-import es.deusto.ingenieria.sd.auctions.server.data.domain.Category;
 import es.deusto.ingenieria.sd.auctions.server.data.domain.SesionEntrenamiento;
 import es.deusto.ingenieria.sd.auctions.server.data.domain.Usuario;
 
 //TODO: Implement Singleton Pattern
 public class SesionesEntrenamientoAppService {
-	
-	//TODO: remove when DAO Pattern is implemented
-	private List<Category> categories = new ArrayList<>();
-	private List<Article> articles = new ArrayList<>();
-	
+
+	// TODO: remove when DAO Pattern is implemented
+	private List<SesionEntrenamiento> sesionesEntrenamiento = new ArrayList<>();
+
 	public SesionesEntrenamientoAppService() {
-		//TODO: remove when DAO Pattern is implemented
+		// TODO: remove when DAO Pattern is implemented
 		this.initilizeData();
 	}
-	
-	//TODO: remove when DAO Pattern is implemented
-	private void initilizeData() {
-		//Create Users
-		Usuario user0 = new Usuario();
-		user0.setEmail("thomas.e2001@gmail.com");
-		user0.setNickname("Thomas");
-		user0.setPassword("$!9PhNz,");
-		
-		Usuario user1 = new Usuario();
-		user1.setEmail("sample@gmail.com");
-		user1.setNickname("buyer33");		
-		user1.setPassword("hqc`}3Hb");
-								
-		//Create Categories
-		Category catPhone = new Category();
-		catPhone.setName("Cell Phones");		
 
-		//Create Articles				
-		Article galaxy = new Article();
-		galaxy.setNumber(9);
-		galaxy.setTitle("Samsung Galaxy S20 128GB");
-		galaxy.setInitialPrice(149.99f);
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DAY_OF_MONTH, 60);
-		galaxy.setAuctionEnd(calendar.getTime());
-		
-		catPhone.addArticle(galaxy);
-		galaxy.setCategory(catPhone);
-		user1.addArticle(galaxy);
-		galaxy.setOwner(user1);
-		
-		Article iphone = new Article();
-		iphone.setNumber(10);
-		iphone.setTitle("Apple iPhone 12 64GB");
-		iphone.setInitialPrice(216.00f);
-		calendar = Calendar.getInstance();
-		calendar.add(Calendar.DAY_OF_MONTH, 30);
-		iphone.setAuctionEnd(calendar.getTime());			
-		
-		catPhone.addArticle(iphone);
-		iphone.setCategory(catPhone);
-		user1.addArticle(iphone);
-		iphone.setOwner(user1);
-						
-		Article xiaomi = new Article();
-		xiaomi.setNumber(11);	
-		xiaomi.setTitle("Xiaomi Mi 10");
-		xiaomi.setInitialPrice(99.40f);
-		calendar = Calendar.getInstance();
-		calendar.add(Calendar.DAY_OF_MONTH, 11);
-		xiaomi.setAuctionEnd(calendar.getTime());			
-				
-		catPhone.addArticle(xiaomi);		
-		xiaomi.setCategory(catPhone);
-		user1.addArticle(xiaomi);
-		xiaomi.setOwner(user1);
-		
-		//Add the Category to the local cache.
-		this.categories.add(catPhone);
-		//Add articles to local cahce
-		this.articles.add(galaxy);
-		this.articles.add(iphone);
-		this.articles.add(xiaomi);
+	// TODO: remove when DAO Pattern is implemented
+	private void initilizeData() {
+		// Create Users
+
 	}
-	
+
 	/////////////////////// METODOS PUBLICOS ///////////////////////
-	
+
 	public boolean crearSesionEntrenamiento(Usuario usuario, SesionEntrenamiento sesionEntrenamiento) {
-		// TODO
+
+		if (!usuario.getSesionesEntrenamiento().contains(sesionEntrenamiento)) {
+			usuario.getSesionesEntrenamiento().add(sesionEntrenamiento);
+			return true;
+		}
+		return false;
+
 	}
-	
+
 	public List<SesionEntrenamiento> getSesionesEntrenamiento(String usuario) {
 		// TODO
 	}
-	
+
 	public boolean eliminarSesionEntrenamiento(String usuario, String sesionEntrenamiento) {
 		// TODO
 	}
-	
+
 	public List<Category> getCategories() {
-		//TODO: Get all the categories using DAO Pattern		
+		// TODO: Get all the categories using DAO Pattern
 		return this.categories;
 	}
 
 	public List<Article> getArticles(String category) {
-		//TODO: Get articles of a category using DAO Pattern
+		// TODO: Get articles of a category using DAO Pattern
 		for (Category cat : this.categories) {
 			if (cat.getName().equalsIgnoreCase(category)) {
 				return cat.getArticles();
 			}
 		}
-		
+
 		return null;
 	}
 
 	public boolean makeBid(Usuario user, int number, float amount) {
-		//TODO: Find the artile using DAO Pattern
+		// TODO: Find the artile using DAO Pattern
 		Article article = null;
-		
+
 		for (Article art : this.articles) {
 			if (art.getNumber() == number) {
 				article = art;
@@ -130,29 +75,40 @@ public class SesionesEntrenamientoAppService {
 		}
 
 		if (article != null) {
-			Bid newBid = new Bid();		
+			Bid newBid = new Bid();
 			newBid.setDate(Calendar.getInstance().getTime());
 			newBid.setAmount(amount);
 			newBid.setArticle(article);
-			newBid.setUser(user);		
+			newBid.setUser(user);
 			article.addBid(newBid);
 			user.addBid(newBid);
 
-			//TODO: Save the new bin in the DB using DAO Pattern
-			
+			// TODO: Save the new bin in the DB using DAO Pattern
+
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	public float getUSDRate() {
-		//TODO: Get conversion rate using Service Gateway
+		// TODO: Get conversion rate using Service Gateway
 		return 0.85f;
 	}
 
 	public float getGBPRate() throws RemoteException {
-		//TODO: Get conversion rate using Service Gateway
+		// TODO: Get conversion rate using Service Gateway
 		return 1.17f;
+	}
+
+	private int getUsuarioPorIndex(String usuario) {
+
+		for (Usuario u : AutenticacionAppService.getInstance().getUsuarios()) {
+			if (u.getNombre().equals(usuario)) {
+				return AutenticacionAppService.getInstance().getUsuarios().indexOf(u);
+			}
+		}
+		return -1;
+
 	}
 }
